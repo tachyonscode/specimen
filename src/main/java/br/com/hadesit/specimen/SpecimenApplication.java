9f6a88d53d4b45e93c1e53c27933bb97cd553fa0
tachyonscode <tachyonscode@gmail.com>
2018-06-03 23:40:28 -0300

    First commit

---
+++ src/main/java/br/com/hadesit/specimen/SpecimenApplication.java
@@ -0,0 +1,12 @@
+package br.com.hadesit.specimen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecimenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecimenApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/hadesit/specimen/SpecimenApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.hadesit.specimen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpecimenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
